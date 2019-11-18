8d08462545f03f0d382220b460424c910757baaf
Prabhakaran Chinnathambi <prabhakaran.chinnathambi@gmail.com>
2019-11-17 23:03:16 -0500

    Different wait strategies

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ Topics covered so far: <br>
 <li>Reflection API</li>
   
 Commit for the above topics: https://github.com/writetoprabha/SeleniumJavaWorkouts/commit/e3fda49d5f1af777a1c3ba9925835c10c55362b2
+
+<h3>Different waits supported in Selenium</h3>
+<h4>Implicit Wait:</h4>
+* it is the default timeout for all the driver.findElement statements.
+* If the element is not located within the given timeout SLA, NoSuchElementFound exception will be thrown
+* driver.manage.timeouts().implicitlyWait(20, TimeUnit.SECONDS); <br>
+* driver.manage.timeouts().pageLoadTimeout(20, TimeUnit.SECONDS); => to make selenium wait for 20 seconds for page load <br>
+* driver.manage.timeouts().setScriptTimeout(20, Timeunit.SECONDS); => to make selenium wait for ajax scripts to execute
+<h4>Explicity Wait</h4>
+* To make selenium wait for an element
+* Sample Code: <br>
+WebDriverWait wait = new WebDriverWait(driver, 10);  => takes the driver and timeout in seconds as input<br>
+wait.until(ExpectedConditions.visibilityOf(driver.findElementBy.Id("xyz")));
+* dynamic in nature => Waits only until the visibility of the element
+<h4>Thread.sleep()<h4>
+* Static wait
+* Should not be used frequently
+<h4>HashSets to fetch different windows opened</h4>
---
+++ src/main/java/WaitStatements.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/*Three types of wait statements are available
+Implicit wait driver.manage.Timeouts.implicitelyWait => sets the timeout for all driver.findElementBy statements
+Explicit wait => WebDriverWait => wait.until(ExpectedConditions.visibilityOfElementLocated
+Static wait => Thread.sleep => should not be used
+ */
+public class WaitStatements {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize(); // => To maximize the window
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(30,TimeUnit.SECONDS);
+
+        driver.get("http://the-internet.herokuapp.com");
+        driver.findElement(By.xpath("//a[contains(text(), 'Dynamic Controls')]")).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'Remove')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[contains(text(), 'Remove')]")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(), 'Add')]")));
+        Thread.sleep(3000); // => static wait. should not be used very offen
+    }
+}
